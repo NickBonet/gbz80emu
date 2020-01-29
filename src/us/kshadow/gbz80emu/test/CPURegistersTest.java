d7c5dca60c26b57794b759b3cdd901a300a71f8e
NickBonet <nbonet@kshadow.us>
2020-01-29 11:07:15 -0500

    Added JUnit test stub for CPURegister class.

---
+++ src/us/kshadow/gbz80emu/test/CPURegistersTest.java
@@ -0,0 +1,14 @@
+package us.kshadow.gbz80emu.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CPURegistersTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
