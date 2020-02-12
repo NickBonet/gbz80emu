5a2aebf7bfc4ff78fe14afa5e654ebff7d892036
NickBonet <nbonet@kshadow.us>
2020-02-12 12:17:00 -0500

    Basic memory abstraction completed.
    Boot ROM mapping, I/O registers, and interrupt register flag will be
    handled later on.

--- src/us/kshadow/gbz80emu/memory/MMU.java
+++ src/us/kshadow/gbz80emu/memory/MMU.java
@@ -1,5 +1,7 @@
 package us.kshadow.gbz80emu.memory;
 
+import us.kshadow.gbz80emu.util.BitUtil;
+
 /**
  * A basic memory management unit abstraction.
  * @author Nicholas Bonet
@@ -10,7 +12,7 @@ public class MMU {
 	private static final MMU instance = new MMU();
 	
 	// TODO: Address space for boot ROM when implemented.
-	// Gets switched out at end of actual Gameboy boot up.
+	// Gets switched out at end of actual Gameboy boot up, when $FF50 is written to.
 	private static int[] bootRom = new int[0xFF];
 	
 	// 0x0000 - 0x3FFF - ROM Bank 0
@@ -43,15 +45,21 @@ public class MMU {
 	
 	// TODO: 0xFFFF - Interrupt Enabled Register
 	
+	private MMU() { }
+	
+	public static MMU getInstance() {
+		return instance;
+	}
+	
 	/**
-	 * handles reading a byte from the correct region of memory, based on memory address.s
+	 * Handles reading a byte from the correct region of memory, based on memory address.
 	 * @param address - Address of the byte to read from memory.
 	 * @return - The byte from memory.
 	 */
 	public int readByte(int address) {
 		switch(address & 0xF000) 
 		{
-			case 0x0000:
+			case 0x0000: // will change later for boot ROM implementation
 			case 0x1000:
 			case 0x2000:
 			case 0x3000:
@@ -89,14 +97,69 @@ public class MMU {
 		}
 	}
 	
+	/**
+	 * Reads 2 bytes from memory arrays, little endian.
+	 * @param address - Address to start reading bytes from.
+	 * @return 2 bytes from memory.
+	 */
 	public int readWord(int address) {
 		return readByte(address) + (readByte(address + 1) << 8);
 	}
 	
-	private MMU() { }
-	
-	public static MMU getInstance() {
-		return instance;
+	/**
+	 * Handles writing a byte to the correct region in memory.
+	 * @param address - Location of where to store value.
+	 * @param value - Value to store.
+	 */
+	public static void writeByte(int address, int value) {
+		BitUtil.checkIsByte(value);
+		switch(address & 0xF000)
+		{
+			case 0x0000:
+			case 0x1000:
+			case 0x2000:
+			case 0x3000:
+			case 0x4000:
+			case 0x5000:
+			case 0x6000:
+			case 0x7000:
+				break; // we don't write to ROM!
+			
+			case 0x8000:
+			case 0x9000:
+				videoRam[address & 0x1FFF] = value;
+				break;
+				
+			case 0xA000:
+			case 0xB000:
+				extRam[address & 0x1FFF] = value;
+				break;
+				
+			case 0xC000:
+			case 0xD000:
+			case 0xE000:
+				workRam[address & 0x1FFF] = value;
+				break;
+				
+			case 0xF000:
+				if (address < 0xFE00) { workRam[address & 0x1FFF] = value; }
+				else if (address >= 0xFE00 && address < 0xFEA0) { oam[address & 0x9F] = value; }
+				else if (address >= 0xFF80 && address < 0xFFFF) { zeroPage[address & 0x7E] = value; }
+				break;
+				
+			default:
+				throw new IllegalArgumentException("Unhandled memory address: " + address);
+		}
 	}
-
+	
+	/**
+	 * Writes a word to desired address in memory, little endian.
+	 * @param address - Address to start writing bytes to.
+	 * @param value - 2 bytes to write.
+	 */
+	public void writeWord(int address, int value) {
+		BitUtil.checkIsWord(value);
+		writeByte(address, value & 0xFF); // least significant bit written first
+		writeByte(address + 1, value >> 8);
+	}
 }
