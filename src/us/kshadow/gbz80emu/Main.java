df5ac1af582c18adde061890a53ccb5e73ca783f
NickBonet <nbonet@kshadow.us>
2020-09-05 15:18:41 -0400

    Maybe start cleaning up for the actual execution loop/opcode matching...

--- src/us/kshadow/gbz80emu/Main.java
+++ src/us/kshadow/gbz80emu/Main.java
@@ -9,58 +9,10 @@ public class Main {
 		
 		// I just use this as a testing area until I make actual unit tests.
 		
-		// instruct_RL test based on manual
-		/*
-		cpu.getCpuRegisters().setL(0x80);
-		cpu.getCpuRegisters().setA(0x11);
-		BitShift.instruct_RL("L");
-		cpu.getCpuRegisters().getFR().setC(false);
-		BitShift.instruct_RL("A");
-		System.out.println("RL Test\n-------------");
-		cpu.getCpuReg().printReg();	
-		*/
 		
-		// instruct_RLA test based on manual
-		cpu.getCpuReg().getFR().setC(true);
-		cpu.getCpuReg().writeReg("A", 0x95, false);
-		BitShift.instructRLA();
-		System.out.println("instruct_RLA");
-		cpu.getCpuReg().printReg();	
-		
-		cpu.getCpuReg().getFR().setFlagsFromByte(0x00);
-		cpu.getCpuReg().writeReg("B", 0x85, false);
-		BitShift.instructRLC("B");
-		System.out.println("\ninstruct_RLC");
-		cpu.getCpuReg().printReg();	
-		cpu.getCpuReg().getFR().setFlagsFromByte(0x00);
-		cpu.getCpuReg().writeReg("H", 0x00, false);
-		BitShift.instructRLC("H");
-		cpu.getCpuReg().printReg();	
-		
-		cpu.getCpuReg().clearRegs();
-		System.out.println("\ninstruct_RR");
-		cpu.getCpuReg().writeReg("A", 0x1, false);
-		cpu.getCpuReg().writeReg("H", 0x8A, false);
-		BitShift.instructRR("A");
-		cpu.getCpuReg().printReg();	
-		cpu.getCpuReg().getFR().setFlagsFromByte(0x00);
-		BitShift.instructRR("H");
-		cpu.getCpuReg().printReg();	
-		
-		cpu.getCpuReg().clearRegs();
-		System.out.println("\ninstruct_RRA");
-		cpu.getCpuReg().writeReg("A", 0x81, false);
-		BitShift.instructRRA();
-		cpu.getCpuReg().printReg();	
-		
-		cpu.getCpuReg().clearRegs();
-		System.out.println("\ninstruct_RRC");
-		cpu.getCpuReg().writeReg("A", 0x1, false);
-		cpu.getCpuReg().writeReg("H", 0x0, false);
-		BitShift.instructRRC("A");
-		cpu.getCpuReg().printReg();	
-		cpu.getCpuReg().getFR().setFlagsFromByte(0x00);
-		BitShift.instructRRC("H");
-		cpu.getCpuReg().printReg();	
+		// Beginning of the actual fetch/decode/execute cycle
+		while(true) {
+			
+		}
 	}
 }
